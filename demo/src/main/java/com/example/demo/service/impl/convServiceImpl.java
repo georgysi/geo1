f1a9fff91d6aa82fe787e75a0f9bd73e05544f4f
U-NTNET\GEORGYS <GEORGYS@georgys03.corp.amdocs.com>
2023-08-18 11:32:39 -0400

    First Commit json to CSV
    	new file:   demo/.gitignore
    	new file:   demo/.mvn/wrapper/maven-wrapper.jar
    	new file:   demo/.mvn/wrapper/maven-wrapper.properties
    	new file:   demo/README
    	new file:   demo/mvnw
    	new file:   demo/mvnw.cmd
    	new file:   demo/pom.xml
    	new file:   demo/short.json
    	new file:   demo/src/main/java/com/example/demo/DemoApplication.java
    	new file:   demo/src/main/java/com/example/demo/rest/convController.java
    	new file:   demo/src/main/java/com/example/demo/service/convService.java
    	new file:   demo/src/main/java/com/example/demo/service/impl/convServiceImpl.java
    	new file:   demo/src/main/resources/application.properties
    	new file:   demo/src/test/java/com/example/demo/DemoApplicationTests.java
    	new file:   demo/test_json.json

---
+++ demo/src/main/java/com/example/demo/service/impl/convServiceImpl.java
@@ -0,0 +1,70 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.convService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.*;
+
+@Service
+@Slf4j
+public class convServiceImpl implements convService {
+
+    @Override
+    public String getCSV() {
+       String jsonString;
+        JSONObject jsonObject;
+
+        try {
+
+            // Step 1: Reading the contents of the JSON file
+            // using readAllBytes() method and
+            // storing the result in a string
+            jsonString = new String(
+                    Files.readAllBytes(Paths.get("short.json")));
+           System.out.print("GEORGY " + jsonString);
+            // Step 2: Construct a JSONObject using above
+            // string
+            jsonObject = new JSONObject(jsonString);
+
+            // Step 3: Fetching the JSON Array test
+            // from the JSON Object
+            JSONArray docs
+                    = jsonObject.getJSONArray("data");
+
+            // Step 4: Create a new CSV file using
+            //  the package java.io.File
+            File file = new File("C:\\Users\\georgys\\Desktop\\workspace\\Test.csv");
+
+            // Step 5: Produce a comma delimited text from
+            // the JSONArray of JSONObjects
+            // and write the string to the newly created CSV
+            // file
+            String csvString = CDL.toString(docs);
+            FileUtils.writeStringToFile(file, csvString);
+        }
+
+        // Catch block to handle exceptions
+        catch (Exception e) {
+
+            // Display exceptions on console with line
+            // number using printStackTrace() method
+            e.printStackTrace();
+        }
+
+        return "CSV downloaded";
+    }
+
+}
